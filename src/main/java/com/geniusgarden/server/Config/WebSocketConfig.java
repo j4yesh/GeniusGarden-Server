c4f4f6f0250a6b93239f0bf6b989a9ae63d0f370
j4yesh <jayeshbadgujar7052@gmail.com>
2024-09-14 22:45:22 +0530

    ioc changes

--- src/main/java/com/geniusgarden/server/Config/WebSocketConfig.java
+++ src/main/java/com/geniusgarden/server/Config/WebSocketConfig.java
@@ -3,6 +3,7 @@ package com.geniusgarden.server.Config;
 import com.geniusgarden.server.Service.GameHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -16,9 +17,13 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     private static final Logger logger = LoggerFactory.getLogger(WebSocketConfig.class);
 
+    @Autowired
+    private GameHandler gameHandler;
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new GameHandler(), "/game/{roomId}").setAllowedOrigins("*");
+        registry.addHandler(gameHandler, "/game/{roomId}/{method}")
+                .setAllowedOrigins("*");
     }
 
 
--- src/main/java/com/geniusgarden/server/Service/GameHandler.java
+++ src/main/java/com/geniusgarden/server/Service/GameHandler.java
@@ -23,19 +23,48 @@ public class GameHandler extends TextWebSocketHandler {
     private static final Logger logger = LoggerFactory.getLogger(GameHandler.class);
     private static final Map<String, Map<String,WebSocketSession>> rooms = new HashMap<>();
     private static final Map<String, String> idNameMap = new HashMap<>();
-
     private final List<Float> spawnPosition = Arrays.asList(-7.0f, 2.0f, 0.0f);
+    private static int playerLimitForRoom = 3;
 
     @Autowired
     questionMaker questionMaker;
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        String conType = getRequestType(session);
         String roomId = getRoomId(session);
+        logger.info(conType + ": connection type.");
+        if ( !rooms.containsKey(roomId) && conType.equals("join")) {
+            payLoad errorPl = new payLoad();
+            errorPl.setType("Error");
+            errorPl.setData("Enter valid room id");
+
+            sendMessageToClientWithoutRoom(session,JsonUtil.toJson(errorPl));
+            session.close();
+            return;
+        }
+        if(rooms.containsKey(roomId) && conType.equals("host")){
+            payLoad errorPl = new payLoad();
+            errorPl.setType("Error");
+            errorPl.setData("please try again to host.");
+            sendMessageToClientWithoutRoom(session,JsonUtil.toJson(errorPl));
+            session.close();
+            return;
+        }
+        if(rooms.containsKey(roomId) && rooms.get(roomId).size()>playerLimitForRoom){
+            payLoad errorPl = new payLoad();
+            errorPl.setType("Error");
+            errorPl.setData("Room is full. (Room limit is 3).");
+            sendMessageToClientWithoutRoom(session,JsonUtil.toJson(errorPl));
+            session.close();
+            return;
+        }
+
         logger.info("New connection to room: " + roomId);
         rooms.computeIfAbsent(roomId, k -> new HashMap<>()).put(session.getId(),session);
         logger.info("sizing : "+rooms.entrySet().size());
-        logger.info("Session added to room: " + roomId);
+
+
 //        rooms.put(new HashMap<>(roomId,new HashMap<>()));
         Map<String,WebSocketSession> sessions = rooms.get(roomId);
 
@@ -44,7 +73,13 @@ public class GameHandler extends TextWebSocketHandler {
         payLoad pl = new payLoad();
         pl.setSocketId(session.getId());
         pl.setType("new player");
-        pl.setPosition(spawnPosition);
+
+
+        Float randX = questionMaker.random.nextFloat(18f) - 9f; // (-9,9)
+        Float randY = questionMaker.random.nextFloat(18f) - 9f;
+        pl.setPosition(Arrays.asList(randX, randY, 0.0f));
+
+
         pl.setName(session.getId());
         this.spawnPosition.set(0,this.spawnPosition.get(0) + 2f);
         pl.setRotation(0f);
@@ -79,7 +114,6 @@ public class GameHandler extends TextWebSocketHandler {
 
 //        logger.info("Message received from room " + roomId + ": " + message.getPayload());
         Map<String, WebSocketSession> roomSessions = rooms.get(roomId);
-
 
         String payload = (String) message.getPayload();
 
@@ -132,7 +166,6 @@ public class GameHandler extends TextWebSocketHandler {
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         String roomId = getRoomId(session);
         Map<String,WebSocketSession> sessions = rooms.get(roomId);;
-
         logger.info("The player is leaving : "+session.getId());
 
         sessions.remove(session.getId());
@@ -178,6 +211,19 @@ public class GameHandler extends TextWebSocketHandler {
             }
         }
     }
+
+    private void sendMessageToClientWithoutRoom(WebSocketSession session, String message) {
+        String roomId = getRoomId(session);
+
+        TextMessage textMessage = new TextMessage(message);
+        try {
+            session.sendMessage(textMessage);
+        } catch (Exception e) {
+            System.err.println("Error sending message to session ID: " + session.getId());
+            e.printStackTrace();
+        }
+    }
+
     private void broadcastMessage(String roomId,String message) {
         Map<String, WebSocketSession> sessions = rooms.get(roomId);
 
@@ -259,7 +305,7 @@ public class GameHandler extends TextWebSocketHandler {
 
                     float randX = questionMaker.random.nextFloat(18f) - 9f; // (-9,9)
                     float randY = questionMaker.random.nextFloat(18f) - 9f;
-
+                    logger.info("randX randY : "+randX+" "+randY);
                     p.setPosition(Arrays.asList(randX, randY, 0f));
                     p.setAnswer(num);
                     try {
@@ -274,7 +320,17 @@ public class GameHandler extends TextWebSocketHandler {
 
     private String getRoomId(WebSocketSession session) {
         String uri = Objects.requireNonNull(session.getUri()).toString();
-        return uri.split("/game/")[1];
+        return uri.split("/")[4];
+    }
+
+    private String getRequestType(WebSocketSession session) {
+        String uri = Objects.requireNonNull(session.getUri()).toString();
+        String[] tempList = uri.split("/");
+        for(String it: tempList){
+            logger.info(it);
+        }
+        return uri.split("/")[5];
+
     }
 
 }
